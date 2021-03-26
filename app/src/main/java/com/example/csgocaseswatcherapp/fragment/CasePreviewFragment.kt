c636340487741559595819e1ccd60f6643a30915
ilya-shevtsov <Dayzilya@gmail.com>
2021-03-26 16:25:25 +0300

    Gradle update

---
+++ app/src/main/java/com/example/csgocaseswatcherapp/fragment/CasePreviewFragment.kt
@@ -0,0 +1,20 @@
+package com.example.csgocaseswatcherapp.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.csgocaseswatcherapp.R
+
+class CasePreviewFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_case_preview2, container, false)
+    }
+
+}
