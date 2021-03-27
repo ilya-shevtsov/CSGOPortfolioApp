ccd9aade9a96f5db4d75c6c78cfbc71a22d4d40e
ilya-shevtsov <Dayzilya@gmail.com>
2021-03-27 23:47:16 +0300

    Added correct display of case info in CaseDetailsFragment

--- app/src/main/java/com/example/csgocaseswatcherapp/presentation/view/fragments/casedetails/CaseDetailsFragment.kt
+++ app/src/main/java/com/example/csgocaseswatcherapp/presentation/view/fragments/casedetails/CaseDetailsFragment.kt
@@ -23,9 +23,9 @@ class CaseDetailsFragment : Fragment(R.layout.fragment_case_details) {
 
         with(binding) {
             caseName.text = args.currentCase.name
-            lowestPrice.text = args.currentCase.lowestPrice.toString()
-            medianPrice.text = args.currentCase.medianPrice.toString()
-            volume.text = args.currentCase.volume.toString()
+            lowestPrice.text = getString(R.string.case_lowest_price, args.currentCase.lowestPrice.toString())
+            medianPrice.text = getString(R.string.case_median_price, args.currentCase.medianPrice.toString())
+            volume.text = getString(R.string.case_volume, args.currentCase.volume.toString())
             caseImage.setImageURI(args.currentCase.imageUrl)
         }
 
