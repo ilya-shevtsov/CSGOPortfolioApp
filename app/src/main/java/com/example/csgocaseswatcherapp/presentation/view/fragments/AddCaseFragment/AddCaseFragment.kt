325db4ecfd7bedba65e1dae1edc2d0b760079299
ilya-shevtsov <Dayzilya@gmail.com>
2021-08-26 00:46:41 +0300

    Add: - hints to the EditText in AddCaseFragment XML - logic for the dropdown menue in AddCaseFragment - string array of the cases in values.strings

--- app/src/main/java/com/example/csgocaseswatcherapp/presentation/view/fragments/AddCaseFragment/AddCaseFragment.kt
+++ app/src/main/java/com/example/csgocaseswatcherapp/presentation/view/fragments/AddCaseFragment/AddCaseFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import com.example.csgocaseswatcherapp.R
 import com.example.csgocaseswatcherapp.databinding.FragmentAddCaseBinding
@@ -16,8 +17,18 @@ class AddCaseFragment : Fragment(R.layout.fragment_add_case) {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-
         binding = FragmentAddCaseBinding.inflate(inflater, container, false)
+
+        val caseNameArray = resources.getStringArray(R.array.case_name_array)
+
+        val adapter =
+            ArrayAdapter(
+                requireContext(),
+                android.R.layout.simple_list_item_1,
+                caseNameArray
+            )
+        binding.caseNameEditText.threshold = 1
+        binding.caseNameEditText.setAdapter(adapter)
 
 
         return binding.root
