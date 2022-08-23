ca91f092b7864f19579c5570a47a23726d32a2af
ilya-shevtsov <Dayzilya@gmail.com>
2022-08-23 21:13:41 +0300

    Add: - PreferredCurrencyDto - connected local server - POST for preferredCurrency - post function in PortfolioFragment

---
+++ app/src/main/java/com/example/csgocaseswatcherapp/data/model/prederredcurrencydto/PreferredCurrencyDto.kt
@@ -0,0 +1,8 @@
+package com.example.csgocaseswatcherapp.data.model.prederredcurrencydto
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class PreferredCurrencyDto(
+    val preferredCurrency: Int,
+)
