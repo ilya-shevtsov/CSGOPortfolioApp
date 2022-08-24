130218be410c2e9f313b7157e18d2913b65fe185
ilya-shevtsov <Dayzilya@gmail.com>
2022-08-24 15:17:55 +0300

    Add: - ic_change_currency_button - dropdown menu for currency change - a lot of style changes themes

--- app/src/main/java/com/example/csgocaseswatcherapp/presentation/view/fragments/portfolio/PortfolioFragment.kt
+++ app/src/main/java/com/example/csgocaseswatcherapp/presentation/view/fragments/portfolio/PortfolioFragment.kt
@@ -5,23 +5,26 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.csgocaseswatcherapp.R
-import com.example.csgocaseswatcherapp.data.api.ApiTools.Companion.getApiService
-import com.example.csgocaseswatcherapp.data.model.prederredcurrencydto.PreferredCurrencyDto
 import com.example.csgocaseswatcherapp.databinding.FragmentPortfolioBinding
 import com.example.csgocaseswatcherapp.presentation.model.caseportfolioitem.PortfolioItem
 import com.example.csgocaseswatcherapp.presentation.model.caseportfolioitem.PortfolioItemGroup
 import com.xwray.groupie.GroupieAdapter
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
 class PortfolioFragment : Fragment(R.layout.fragment_portfolio) {
 
     private lateinit var binding: FragmentPortfolioBinding
     private val caseListAdapter = GroupieAdapter()
+
+    override fun onResume() {
+        super.onResume()
+        val currencies = resources.getStringArray(R.array.currency_array)
+        val arrayAdapter = ArrayAdapter(requireContext(),R.layout.dropdown_item,currencies)
+        binding.autoCompleteTextView.setAdapter(arrayAdapter)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -92,27 +95,28 @@ class PortfolioFragment : Fragment(R.layout.fragment_portfolio) {
                 )
             )
         )
-
-
-
         caseListAdapter.add(portfolioItemGroup)
 
-        binding.currencyChangeButtonUSD.setOnClickListener {
-            val prefCur = PreferredCurrencyDto(1)
-            CoroutineScope(Dispatchers.IO).launch {
-                getApiService().postPreferredCurrency(prefCur)
-                Log.e("M_PortfolioFragment.post", "USD")
-            }
-//
-        }
-        binding.currencyChangeButtonRUB.setOnClickListener {
-            val prefCur = PreferredCurrencyDto(5)
-            CoroutineScope(Dispatchers.IO).launch {
-                getApiService().postPreferredCurrency(prefCur)
-                Log.e("M_PortfolioFragment.post", "RUB")
-            }
+        val currencies = resources.getStringArray(R.array.currency_array)
+        val arrayAdapter = ArrayAdapter(requireContext(),R.layout.dropdown_item,currencies)
+        binding.autoCompleteTextView.setAdapter(arrayAdapter)
 
-        }
+//        binding.currencyChangeButtonUSD.setOnClickListener {
+//            val prefCur = PreferredCurrencyDto(1)
+//            CoroutineScope(Dispatchers.IO).launch {
+//                getApiService().postPreferredCurrency(prefCur)
+//                Log.e("M_PortfolioFragment.post", "USD")
+//            }
+//
+//        }
+//        binding.currencyChangeButtonRUB.setOnClickListener {
+//            val prefCur = PreferredCurrencyDto(5)
+//            CoroutineScope(Dispatchers.IO).launch {
+//                getApiService().postPreferredCurrency(prefCur)
+//                Log.e("M_PortfolioFragment.post", "RUB")
+//            }
+//
+//        }
 
         binding.homeButton.setOnClickListener {
             findNavController().navigate(R.id.startFragment)
