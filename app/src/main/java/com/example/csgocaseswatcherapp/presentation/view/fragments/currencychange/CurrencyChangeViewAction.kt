5a4ef0465283352a7fb966912aedfd085d2339c2
ilya-shevtsov <Dayzilya@gmail.com>
2022-08-27 19:20:03 +0300

    Add: - CurrencyChange to MVVM

---
+++ app/src/main/java/com/example/csgocaseswatcherapp/presentation/view/fragments/currencychange/CurrencyChangeViewAction.kt
@@ -0,0 +1,9 @@
+package com.example.csgocaseswatcherapp.presentation.view.fragments.currencychange
+
+sealed class CurrencyChangeViewAction {
+
+    data class OnCurrencyClicked(
+        val preferredCurrency: String
+    ) : CurrencyChangeViewAction()
+
+}
--- app/src/main/java/com/example/csgocaseswatcherapp/presentation/view/fragments/currencychange/CurrencyChangeViewModel.kt
+++ app/src/main/java/com/example/csgocaseswatcherapp/presentation/view/fragments/currencychange/CurrencyChangeViewModel.kt
@@ -1,17 +1,38 @@
 package com.example.csgocaseswatcherapp.presentation.view.fragments.currencychange
 
 import androidx.lifecycle.ViewModel
-import com.example.csgocaseswatcherapp.presentation.view.fragments.start.StartViewState
+import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
 
-class CurrencyChangeViewModel : ViewModel(){
+class CurrencyChangeViewModel : ViewModel() {
 
     val uiState = MutableStateFlow(value = createInitialState())
 
     val uiEvent = MutableSharedFlow<CurrencyChangeViewEvent>()
 
+    fun handleAction(action: CurrencyChangeViewAction) {
+        when (action) {
+            is CurrencyChangeViewAction.OnCurrencyClicked -> handleCurrencyItemClicked(action.preferredCurrency)
+        }
 
-    private fun createInitialState(): CurrencyChangeViewState = CurrencyChangeViewState(listOf("USD","RUB"))
+    }
+
+    private fun handleCurrencyItemClicked(currencyName: String) {
+        viewModelScope.launch {
+            uiEvent.emit(
+                CurrencyChangeViewEvent.NavigateToStartWithPreferredCurrency(
+                    currencyName
+                )
+            )
+        }
+    }
+
+
+    //PlaceHolder for currencies (later get from database)
+
+    private fun createInitialState(): CurrencyChangeViewState =
+        CurrencyChangeViewState(listOf("USD", "RUB"))
 
 }
