d15f95cd771ff0863f41258b4e94b4258e2e87b9
ilya-shevtsov <Dayzilya@gmail.com>
2022-09-14 14:50:24 +0300

    Added: - setUpChart with(binding)

--- app/src/main/java/com/example/csgocaseswatcherapp/presentation/view/fragments/portfolio/PortfolioFragment.kt
+++ app/src/main/java/com/example/csgocaseswatcherapp/presentation/view/fragments/portfolio/PortfolioFragment.kt
@@ -117,44 +117,48 @@ class PortfolioFragment : Fragment(R.layout.fragment_portfolio) {
     }
 
     private fun setUpChart() {
-        binding.barChartPortfolioValue.description.isEnabled = false
-        binding.barChartPortfolioValue.legend.isEnabled = false
 
-        val newDataList = listOf(
-            BarEntry(1f, 129f),
-            BarEntry(2f, 164f),
-            BarEntry(3f, 225f),
-            BarEntry(4f, 236f),
-            BarEntry(5f, 334f),
-            BarEntry(6f, 479f),
-            BarEntry(7f, 429f),
-            BarEntry(8f, 424f),
-            BarEntry(9f, 448f),
-            BarEntry(10f, 335f),
-            BarEntry(11f, 315f),
-            BarEntry(12f, 322f),
-            BarEntry(13f, 414f),
-            BarEntry(14f, 458f),
-            BarEntry(15f, 509f),
-            BarEntry(16f, 546f),
-            BarEntry(17f, 668f),
-            BarEntry(18f, 741f),
-            BarEntry(19f, 685f),
-            BarEntry(20f, 840f),
-            BarEntry(21f, 834f),
-        )
+        with(binding) {
 
-        val dataSet = BarDataSet(newDataList, "Portfolio Value")
-        dataSet.color = Color.parseColor("#2FA1BA")
+            barChartPortfolioValue.description.isEnabled = false
+            barChartPortfolioValue.legend.isEnabled = false
 
-        val data = BarData(dataSet)
-        data.setDrawValues(true)
-        data.setValueTextSize(10f)
-        data.setValueTextColor(Color.BLACK)
+            val newDataList = listOf(
+                BarEntry(1f, 129f),
+                BarEntry(2f, 164f),
+                BarEntry(3f, 225f),
+                BarEntry(4f, 236f),
+                BarEntry(5f, 334f),
+                BarEntry(6f, 479f),
+                BarEntry(7f, 429f),
+                BarEntry(8f, 424f),
+                BarEntry(9f, 448f),
+                BarEntry(10f, 335f),
+                BarEntry(11f, 315f),
+                BarEntry(12f, 322f),
+                BarEntry(13f, 414f),
+                BarEntry(14f, 458f),
+                BarEntry(15f, 509f),
+                BarEntry(16f, 546f),
+                BarEntry(17f, 668f),
+                BarEntry(18f, 741f),
+                BarEntry(19f, 685f),
+                BarEntry(20f, 840f),
+                BarEntry(21f, 834f),
+            )
 
-        binding.barChartPortfolioValue.data = data
-        binding.barChartPortfolioValue.invalidate()
-        binding.barChartPortfolioValue.animateY(1400, Easing.EaseInOutQuad)
+            val dataSet = BarDataSet(newDataList, "Portfolio Value")
+            dataSet.color = Color.parseColor("#2FA1BA")
+
+            val data = BarData(dataSet)
+            data.setDrawValues(true)
+            data.setValueTextSize(10f)
+            data.setValueTextColor(Color.BLACK)
+
+            barChartPortfolioValue.data = data
+            barChartPortfolioValue.invalidate()
+            barChartPortfolioValue.animateY(1400, Easing.EaseInOutQuad)
+        }
     }
 
     private fun handleEvent(uiEvent: PortfolioViewEvent) {
