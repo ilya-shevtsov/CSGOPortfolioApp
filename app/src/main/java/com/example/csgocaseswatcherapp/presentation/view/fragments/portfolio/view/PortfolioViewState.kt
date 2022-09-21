9b07a3e9be0c68e3e773e489ce44640e733516af
ilya-shevtsov <Dayzilya@gmail.com>
2022-09-21 17:57:55 +0300

    Added: - Dagger Error is no more

--- app/src/main/java/com/example/csgocaseswatcherapp/presentation/view/fragments/portfolio/view/PortfolioViewState.kt
+++ app/src/main/java/com/example/csgocaseswatcherapp/presentation/view/fragments/portfolio/view/PortfolioViewState.kt
@@ -1,10 +1,8 @@
-package com.example.csgocaseswatcherapp.presentation.view.fragments.portfolio
+package com.example.csgocaseswatcherapp.presentation.view.fragments.portfolio.view
 
 import com.example.csgocaseswatcherapp.presentation.model.caseportfolioitem.PortfolioCaseItem
-import com.example.csgocaseswatcherapp.presentation.model.caseportfolioitem.PortfolioGroupieItem
 import com.example.csgocaseswatcherapp.presentation.model.portfoliodetailsvalueitem.PortfolioValueItem
 import com.github.mikephil.charting.data.BarEntry
-import com.github.mikephil.charting.data.PieEntry
 
 sealed class PortfolioViewState {
 
