c2f203db3071c3f701ab1a3c756bece5888bab3d
Ilia <dayzilya@gmail.com>
2025-08-25 17:43:47 +0400

    removed ActionBar

--- app/src/main/java/com/example/csgocaseswatcherapp/MainActivity.kt
+++ app/src/main/java/com/example/csgocaseswatcherapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.csgocaseswatcherapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.WindowCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupActionBarWithNavController
 
@@ -9,9 +10,10 @@ open class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, true)
         setContentView(R.layout.activity_main)
 
-        setupActionBarWithNavController(findNavController(R.id.fragment))
+//        setupActionBarWithNavController(findNavController(R.id.fragment))
     }
 
     override fun onSupportNavigateUp(): Boolean {
