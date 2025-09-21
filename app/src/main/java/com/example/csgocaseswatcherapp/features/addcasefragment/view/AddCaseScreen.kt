61ed6453c9910c4a83dbef1869256509c51e5cb6
Ilia <dayzilya@gmail.com>
2025-09-21 20:05:06 +0400

    test

--- app/src/main/java/com/example/csgocaseswatcherapp/features/addcasefragment/view/AddCaseScreen.kt
+++ app/src/main/java/com/example/csgocaseswatcherapp/features/addcasefragment/view/AddCaseScreen.kt
@@ -8,9 +8,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
@@ -58,6 +56,7 @@ fun AddCaseScreen(
     }
 }
 
+//test
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AddCaseContent(
