b1c5fed3213835ccd68ff57b1ed48fec7800059d
Ilia <dayzilya@gmail.com>
2025-09-24 19:37:15 +0400

    Fixed import

--- app/src/main/java/com/example/csgocaseswatcherapp/core/navigation/CustomNavType.kt
+++ app/src/main/java/com/example/csgocaseswatcherapp/core/navigation/CustomNavType.kt
@@ -1,13 +1,13 @@
 package com.example.csgocaseswatcherapp.core.navigation
 
+import com.example.csgocaseswatcherapp.core.serialization.NavArgCodec.listSerializer
 import com.example.csgocaseswatcherapp.core.serialization.navTypeOf
 import com.example.csgocaseswatcherapp.features.caseoverview.view.entities.CaseOverviewModel
 import com.example.csgocaseswatcherapp.features.portfolio.view.entities.PortfolioItem
-import kotlinx.serialization.builtins.ListSerializer
 
 
 object CustomNavType {
     val CaseOverviewModelType = navTypeOf(CaseOverviewModel.serializer())
 
-    val PortfolioItemListType = navTypeOf(ListSerializer(PortfolioItem.serializer()))
+    val PortfolioItemListType = navTypeOf(listSerializer(PortfolioItem.serializer()))
 }
