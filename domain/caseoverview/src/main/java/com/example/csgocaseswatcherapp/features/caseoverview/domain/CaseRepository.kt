deca50f801ed852f78c10cde13ec1d7c92780fd1
Ilia <dayzilya@gmail.com>
2026-04-29 21:48:59 +0400

    Moved view,domain,data for caseoverview into a feature structure

--- domain/caseoverview/src/main/java/com/example/csgocaseswatcherapp/features/caseoverview/domain/CaseRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.example.csgocaseswatcherapp.features.caseoverview.domain
-
-import com.example.csgocaseswatcherapp.features.caseoverview.domain.entities.CaseOverview
-
-
-interface CaseRepository {
-    suspend fun getCaseOverviewList(): List<CaseOverview>
-}
