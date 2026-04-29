3bb0b0a15eb7ccaa34c3761fddbdae037a8b0736
Ilia <dayzilya@gmail.com>
2026-04-29 21:51:59 +0400

    Moved view,domain,data for portfolio into a feature structure

--- feature/portfolio/src/main/java/com/example/csgocaseswatcherapp/features/portfolio/view/sortingmodal/entities/SortingEntry.kt
+++
@@ -1,6 +0,0 @@
-package com.example.csgocaseswatcherapp.features.portfolio.view.sortingmodal.entities
-
-data class SortingEntry(
-    val name:String,
-    val method: SortState
-)
--- feature/portfolio/src/main/java/com/example/csgocaseswatcherapp/features/portfolio/view/sortingmodal/view/SortingModalViewState.kt
+++
@@ -1,7 +0,0 @@
-package com.example.csgocaseswatcherapp.features.portfolio.view.sortingmodal.view
-
-import com.example.csgocaseswatcherapp.features.portfolio.view.sortingmodal.entities.SortingEntry
-
-data class SortingModalViewState(
-    val sortingEntryList:List<SortingEntry>
-)
