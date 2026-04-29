59788d8d86db5942f5cccb58af9d3c3e4f22ce68
Ilia <dayzilya@gmail.com>
2026-04-29 21:53:49 +0400

    Moved view,domain,data for start screen into a feature structure

--- data/start/src/main/java/com/example/csgocaseswatcherapp/features/start/data/entities/PreferredCurrencyDto.kt
+++
@@ -1,8 +0,0 @@
-package com.example.csgocaseswatcherapp.features.start.data.entities
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class PreferredCurrencyDto(
-    val preferredCurrency: Int,
-)
--- domain/start/src/main/java/com/example/csgocaseswatcherapp/features/start/domain/entities/PreferredCurrency.kt
+++
@@ -1,5 +0,0 @@
-package com.example.csgocaseswatcherapp.features.start.domain.entities
-
-data class PreferredCurrency(
-    val preferredCurrency: Int,
-)
