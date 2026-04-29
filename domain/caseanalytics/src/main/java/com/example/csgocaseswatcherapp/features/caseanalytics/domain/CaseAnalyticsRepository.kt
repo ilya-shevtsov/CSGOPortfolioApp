64139f8b73f042e2c927302e9eabd3dadcb4c06b
Ilia <dayzilya@gmail.com>
2026-04-29 21:42:02 +0400

    Moved view,domain,data for caseanalytics into a feature structure

--- domain/caseanalytics/src/main/java/com/example/csgocaseswatcherapp/features/caseanalytics/domain/CaseAnalyticsRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.example.csgocaseswatcherapp.features.caseanalytics.domain
-
-import com.example.csgocaseswatcherapp.features.caseanalytics.domain.entities.CaseAnalytics
-
-interface CaseAnalyticsRepository {
-    suspend fun getCaseAnalyticsList(): List<CaseAnalytics>
-}
