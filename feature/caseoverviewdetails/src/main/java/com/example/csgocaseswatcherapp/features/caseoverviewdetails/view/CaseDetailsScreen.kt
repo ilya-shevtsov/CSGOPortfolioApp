3ea108797a0c5d5d3f4fccc700250d20a89e245b
Ilia <dayzilya@gmail.com>
2026-05-11 19:22:06 +0400

    Migrate Coil to Coil 3

--- feature/caseoverviewdetails/src/main/java/com/example/csgocaseswatcherapp/features/caseoverviewdetails/view/CaseDetailsScreen.kt
+++ feature/caseoverviewdetails/src/main/java/com/example/csgocaseswatcherapp/features/caseoverviewdetails/view/CaseDetailsScreen.kt
@@ -27,8 +27,9 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
-import coil.compose.AsyncImage
-import coil.request.ImageRequest
+import coil3.compose.AsyncImage
+import coil3.request.ImageRequest
+import coil3.request.crossfade
 import com.example.csgocaseswatcherapp.core.ui.LoadingScreen
 import com.example.csgocaseswatcherapp.core.ui.theme.AppTheme
 import com.example.csgocaseswatcherapp.features.caseoverviewdetails.R
