ce025e1b7fc68ac00381fb002ec7c4d309ba5739
Ilia <dayzilya@gmail.com>
2026-05-11 19:06:58 +0400

    Update adaptive window size logic

--- core/ui/src/main/java/com/example/csgocaseswatcherapp/core/ui/adaptive/DeviceConfigurationType.kt
+++ core/ui/src/main/java/com/example/csgocaseswatcherapp/core/ui/adaptive/DeviceConfigurationType.kt
@@ -1,8 +1,6 @@
 package com.example.csgocaseswatcherapp.core.ui.adaptive
 
-import androidx.window.core.layout.WindowHeightSizeClass
 import androidx.window.core.layout.WindowSizeClass
-import androidx.window.core.layout.WindowWidthSizeClass
 
 enum class DeviceConfigurationType {
     MOBILE_PORTRAIT,
@@ -10,21 +8,12 @@ enum class DeviceConfigurationType {
 
     companion object {
         fun fromWindowSizeClass(windowSizeClass: WindowSizeClass): DeviceConfigurationType {
-            val widthClass = windowSizeClass.windowWidthSizeClass
-            val heightClass = windowSizeClass.windowHeightSizeClass
+            val isExpandedWidth = windowSizeClass.isWidthAtLeastBreakpoint(840)
+            val isCompactHeight = !windowSizeClass.isHeightAtLeastBreakpoint(480)
 
             return when {
-                widthClass == WindowWidthSizeClass.COMPACT &&
-                        heightClass == WindowHeightSizeClass.MEDIUM -> MOBILE_PORTRAIT
-
-                widthClass == WindowWidthSizeClass.COMPACT &&
-                        heightClass == WindowHeightSizeClass.EXPANDED -> MOBILE_PORTRAIT
-
-                widthClass == WindowWidthSizeClass.EXPANDED &&
-                        heightClass == WindowHeightSizeClass.COMPACT -> MOBILE_LANDSCAPE
-
-                else -> MOBILE_PORTRAIT //TODO: stub for tablet & desktop
-
+                isExpandedWidth && isCompactHeight -> MOBILE_LANDSCAPE
+                else -> MOBILE_PORTRAIT
             }
         }
     }
